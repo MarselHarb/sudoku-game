82e8b6c884ae42ecd48c66dc4c2a8a19231f6ae0
Marsel <smurf.harb@gmail.com>
2024-06-06 12:22:37 +0300

    update frontEnd

--- src/soduku/Cell.java
+++ src/soduku/Cell.java
@@ -11,10 +11,42 @@ public class Cell extends JTextField{
     public static final Color BACKGROUND_Given = new Color(240,240,240);
     public static final Color FOREGROUND_Given = Color.BLACK;
     public static final Color BACKGROUND_Guess = Color.YELLOW;
+    public static final Color FOREGROUND_NOT_Given =Color.GRAY;
     public static final Color BACKGROUND_Correct = new Color(0,216,0);
     public static final Color BACKGROUND_Wrong = new Color (216,0,0);
-    public static final Font FONT_Numbers =
+    public static final Font FONT_Numbers = new Font("OCR A Extenede",Font.PLAIN,28);
 
+    int row,col;
+    int number;
+    CellStatus status;
 
+    public Cell(int row, int col){
+        super();
+        this.row=row;
+        this.col=col;
+    super.setHorizontalAlignment(JTextField.CENTER);
+    super.setFont(FONT_Numbers);
+    }
+    public void newGame(int number, boolean isGiven) {
+        this.number = number;
+    }
+
+    public void paint(){
+        if(status == CellStatus.GIVEN) {
+            super.setText(number + "");
+            super.setEditable(false);
+            super.setBackground(BACKGROUND_Given);
+            super.setForeground(FOREGROUND_Given);
+        } else if (status == CellStatus.GUESS){
+            super.setText("");
+            super.setEditable(true);
+            super.setBackground(BACKGROUND_Guess);
+            super.setForeground(FOREGROUND_NOT_Given);
+        } else if (status == CellStatus.CORRECT){
+            super.setBackground(BACKGROUND_Correct);
+        } else if (status == CellStatus.WRONG){
+            super.setBackground(BACKGROUND_Wrong);
+        }
+    }
 }
 
---
+++ src/soduku/Puzzle.java
@@ -0,0 +1,33 @@
+package soduku;
+
+public class Puzzle {
+    int [][] numbers = new int[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];
+    boolean[][] isGiven = new boolean[SudokuConstants.GRID_SIZE][SudokuConstants.GRID_SIZE];// IF code does not work, try to use SUBGRID_SIZE
+
+    public Puzzle(){
+        super();
+    }
+
+    public void newPuzzle(int cellsToGuess){
+        int [][] gridExample = {
+                {5, 1, 9, 7, 2, 6, 8, 3, 4},
+                {2, 8, 6, 3, 4, 5, 1, 9, 7},
+                {3, 7, 4, 9, 8, 1, 5, 2, 6},
+                {4, 3, 8, 5, 9, 2, 6, 7, 1},
+                {6, 9, 2, 4, 7, 8, 3, 1, 5},
+                {7, 5, 1, 6, 3, 9, 4, 8, 2},
+                {8, 6, 7, 2, 5, 3, 9, 4, 1},
+                {1, 4, 3, 8, 6, 7, 2, 5, 9},
+                {9, 2, 5, 1, 1, 4, 7, 6, 8}};
+
+        for (int row = 0;row < SudokuConstants.GRID_SIZE;row++){    // If code does not work, try ++row instead of row++
+             for (int col = 0; col<SudokuConstants.GRID_SIZE; col ++){
+                 numbers[row][col]=gridExample[row][col];
+             }
+        }
+
+
+
+    }
+
+}
